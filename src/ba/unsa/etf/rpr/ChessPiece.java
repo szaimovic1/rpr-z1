997cb87cb5cf6aaa2f84ba642f067a099d1c411b
szaimovic1 <szaimovic1@etf.unsa.ba>
2018-11-06 01:03:08 +0100

    Dodana klasa Rook.

--- src/ba/unsa/etf/rpr/ChessPiece.java
+++ src/ba/unsa/etf/rpr/ChessPiece.java
@@ -1,29 +1,29 @@
 package ba.unsa.etf.rpr;
 
 public abstract class ChessPiece {
-    public boolean nepostojecaPozicija(String position){
+    boolean nepostojecaPozicija(String position){                                       //default package private????
         if(position.length()<=1) return true;
         if(position.charAt(0) < 'A' || (position.charAt(0)  > 'H' && position.charAt(0) < 'a')
                 || position.charAt(0) > 'h')
             return true;
-        if(position.charAt(1)  < '1' || position.charAt(1) > '8')                        //moze i pozicija da se koristi
+        if(position.charAt(1)  < '1' || position.charAt(1) > '8')                       //moze i pozicija da se koristi
             return true;
         return false;
     }
 
-   // ChessPiece() throws IllegalChessMoveException {};                //treba li??
+   // ChessPiece() throws IllegalChessMoveException {};                                //treba li??
 
     public static  enum Color{
         BLACK, WHITE
 
         /*private String boja;
-        Colour(String boja){                                           //razmotriti u nastavku po potrebi
-            this.boja=boja;                                            //i moze li ovako uopste???
+        Colour(String boja){                                                          //razmotriti u nastavku po potrebi
+            this.boja=boja;                                                           //i moze li ovako uopste???
         }
 
         public String dajBoju(){
             return boja;
-        }*/
+        }*/                                                                           //sta sa upozorenjima???!!!!!!
     }
     public abstract String getPosition();
     public abstract Color getColour();
