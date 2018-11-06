d55af9c8c741bd504545fa3f79ca3b4806b446e2
szaimovic1 <szaimovic1@etf.unsa.ba>
2018-11-06 01:23:48 +0100

    Dodana klasa Bishop.

---
+++ src/ba/unsa/etf/rpr/Bishop.java
@@ -0,0 +1,47 @@
+package ba.unsa.etf.rpr;
+
+public class Bishop extends ChessPiece{
+    private Color boja;
+    private String pozicija;
+
+    private boolean nedozvoljenaPozicija(String pozicija){
+        if((this.pozicija.charAt(1)=='2' && boja==Color.WHITE) &&
+                (this.pozicija.charAt(1)+1==pozicija.charAt(1) ||
+                 this.pozicija.charAt(1)+2==pozicija.charAt(1)))
+            return false;
+        if((this.pozicija.charAt(1)=='7' && boja==Color.BLACK) &&
+                (this.pozicija.charAt(1)-1==pozicija.charAt(1) ||
+                 this.pozicija.charAt(1)-2==pozicija.charAt(1)))
+            return false;
+        if((this.pozicija.charAt(1)+1==pozicija.charAt(1) && boja==Color.WHITE)
+           || (this.pozicija.charAt(1)-1==pozicija.charAt(1) && boja==Color.BLACK))
+            return false;
+        return true;                                                          //zanemarena mogucnost vracanja figure!!!!
+    }
+
+    Bishop (String pozicija, Color boja){
+        if(nepostojecaPozicija(pozicija))
+            throw new IllegalArgumentException("Nepostojeća pozicija!\n");
+        this.boja=boja;
+        this.pozicija=pozicija;
+    }
+
+    @Override
+    public Color getColour() {
+        return boja;
+    }
+
+    @Override
+    public String getPosition() {
+        return pozicija;
+    }
+
+    @Override
+    public void move(String position) throws IllegalChessMoveException{
+        if(nepostojecaPozicija(position))
+            throw new IllegalArgumentException("Nepostojeća pozicija!\n");
+        if(nedozvoljenaPozicija(position))
+            throw new IllegalChessMoveException("Nedozvoljena pozicija!\n");
+        pozicija=position;
+    }
+}
