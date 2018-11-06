834f5d38bd86e7a5d88f426b3ccde1a3dfe95854
szaimovic1 <szaimovic1@etf.unsa.ba>
2018-11-06 01:51:00 +0100

    Dodana klasa Knight. Trenutno napisane klase, prolaze trenutno dostupne testove.

---
+++ src/ba/unsa/etf/rpr/Knight.java
@@ -0,0 +1,41 @@
+package ba.unsa.etf.rpr;
+
+public class Knight extends  ChessPiece{
+    private ChessPiece.Color boja;
+    private String pozicija;
+
+    private boolean nedozvoljenaPozicija(String pozicija){
+        if(((this.pozicija.charAt(0)+1==pozicija.charAt(0) || this.pozicija.charAt(0)-1==pozicija.charAt(0)) &&
+                (this.pozicija.charAt(1)+2==pozicija.charAt(1) || this.pozicija.charAt(1)-2==pozicija.charAt(1)))
+            || ((this.pozicija.charAt(1)+1==pozicija.charAt(1) || this.pozicija.charAt(1)-1==pozicija.charAt(1)) &&
+                (this.pozicija.charAt(0)+2==pozicija.charAt(0) || this.pozicija.charAt(0)-2==pozicija.charAt(0))))
+            return false;                                                //upozorenje-can be simplified..
+        return true;                                                     //kako najlakse razmotriti sve kombinacije?????
+    }
+
+    Knight(String pozicija, ChessPiece.Color boja){
+        if(nepostojecaPozicija(pozicija))
+            throw new IllegalArgumentException("Nepostojeća pozicija!\n");
+        this.boja=boja;
+        this.pozicija=pozicija;
+    }
+
+    @Override
+    public ChessPiece.Color getColour() {
+        return boja;
+    }
+
+    @Override
+    public String getPosition() {
+        return pozicija;
+    }
+
+    @Override
+    public void move(String position) throws IllegalChessMoveException{
+        if(nepostojecaPozicija(position))
+            throw new IllegalArgumentException("Nepostojeća pozicija!\n");
+        if(nedozvoljenaPozicija(position))
+            throw new IllegalChessMoveException("Nedozvoljena pozicija!\n");
+        pozicija=position;
+    }
+}
